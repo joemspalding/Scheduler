26db4468c6237e74ea4a54470bed6eade65148b2
Joe Spalding <jmspal11@louisville.edu>
2017-03-20 09:52:49 -0400

    Organize files and folders

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Need $.when to get both files.
 $.when(
-    $.ajax("dataTerm.json"),
-    $.ajax("dataSubject.json")
+    $.ajax("python/dataTerm.json"),
+    $.ajax("python/dataSubject.json")
 ). done(function(term, subject) {
-    console.log(term[0]);
+    console.log(term[0,1]);
     console.log(subject[0]);
 })
